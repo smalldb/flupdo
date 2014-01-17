fe6590c640481a92f31e8a95aaf0bcccb4c08c73
Josef Kufner <jk@frozen-doe.net>
2014-01-17 01:49:49 +0100

    Fix tests

--- test/init.php
+++ test/init.php
@@ -16,16 +16,6 @@
  *
  */
 
-spl_autoload_register(function($class) {
-	$dir = dirname(__FILE__).'/../class/';
-	$lc_class = strtolower($class);
-        @ list($head, $tail) = explode("\\", $lc_class, 2);
-
-	if ($head == 'smalldb') {
-		include($dir.str_replace('\\', '/', $tail).'.php');
-	}
-});
-
 
 /**
  * Print SQL result in a nice table
